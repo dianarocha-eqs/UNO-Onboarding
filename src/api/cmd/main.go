570eec43cb22107b61fe723584204f89298e2d8a
dianarocha-eqs <diana.rocha@eqsglobal.com>
2025-01-24 11:15:41 +0000

    changed main

--- src/api/cmd/main.go
+++ src/api/cmd/main.go
@@ -4,7 +4,9 @@ import (
 	"log"
 
 	routes_s "api/internal/sensors"
-	routes_u "api/internal/users"
+	"api/internal/sensors/handler"
+	"api/internal/sensors/repository"
+	"api/internal/sensors/usecase"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -14,9 +16,23 @@ func main() {
 
 	router := gin.Default()
 	router.Use(cors.Default())
+
+	var repos repository.SensorRepository
+	var service usecase.SensorService
+	var h handler.SensorHandler
+	var err error
+
+	repos, err = repository.NewSensorRepository()
+
+	if err != nil {
+		log.Fatalf("Failed to create repository: %v", err)
+	}
+	service = usecase.NewSensorService(repos)
+
+	h = handler.NewSensorHandler(service)
+
 	// Sensor routes
-	routes_s.RegisterSensorRoutes(router)
-	routes_u.RegisterUsersRoutes(router)
+	routes_s.RegisterSensorRoutes(router, h)
 
 	log.Println("Server is running on :8080...")
 	if err := router.Run(":8080"); err != nil {
