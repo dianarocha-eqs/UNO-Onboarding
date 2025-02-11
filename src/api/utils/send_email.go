d141b0dff8e2322d23486ef9a75cf982709c5160
dianarocha-eqs <diana.rocha@eqsglobal.com>
2025-02-11 15:48:45 +0000

    json with email configuration; move sendEmail to utils

--- src/api/utils/send_email.go
+++ src/api/utils/send_email.go
@@ -2,13 +2,15 @@ package utils
 
 import (
 	"api/configs"
+	"api/internal/users/domain"
+	"errors"
 	"fmt"
 
 	"gopkg.in/gomail.v2"
 )
 
 // Send sends an email with the given recipient, subject, and content
-func SendEmail(to, subject, body string) error {
+func CreateEmail(to, subject, body string) error {
 	// Load email configuration
 	config, err := configs.LoadConfig()
 	if err != nil {
@@ -30,4 +32,18 @@ func SendEmail(to, subject, body string) error {
 	dialer := gomail.NewDialer(config.Email.Host, config.Email.Port, config.Email.From, config.Email.Password)
 	return dialer.DialAndSend(msg)
 
+}
+
+func SendEmail(user *domain.User, plainPassword string) error {
+	// Send the plain password to the user's email
+	var emailSubject string
+	var emailBody string
+	emailSubject = "Welcome to UNO Service"
+	emailBody = fmt.Sprintf("Hello %s,\n\nYour account has been created. Your temporary password is: %s\n\nPlease change it after logging in.", user.Name, plainPassword)
+
+	err := CreateEmail(user.Email, emailSubject, emailBody)
+	if err != nil {
+		return errors.New("user created but failed to send email")
+	}
+	return nil
 }
