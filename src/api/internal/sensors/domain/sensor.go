7a8abdfd5da595c3d0e7b898df8b7df541496cf4
dianarocha-eqs <diana.rocha@eqsglobal.com>
2025-02-18 17:58:24 +0000

    changes after own review

--- src/api/internal/sensors/domain/sensor.go
+++ src/api/internal/sensors/domain/sensor.go
@@ -11,16 +11,10 @@ const (
 	PUBLIC  bool = false
 	PRIVATE bool = true
 
-	// Predefined categories constants
-	TEMPERATURE string = "Temperature"
-	HUMIDITY    string = "Humidity"
-	PRESSURE    string = "Pressure"
-
-	// Predefined colors constants
-	RED    string = "Red"
-	GREEN  string = "Green"
-	BLUE   string = "Blue"
-	YELLOW string = "Yellow"
+	// Category constants
+	TEMPERATURE int = 0
+	HUMIDITY    int = 1
+	PRESSURE    int = 2
 )
 
 // Sensor represents a device that collects and transmits data about its environment.
@@ -30,13 +24,13 @@ type Sensor struct {
 	// Name of the sensor
 	Name string `json:"name" gorm:"column:name;type:nvarchar(100);not null"`
 	// Category specifies the type of data the sensor collects
-	Category string `json:"category" gorm:"column:category;type:nvarchar(20);not null"`
-	// Color for the sensor to visually identify it
-	Color string `json:"color" gorm:"column:color;type:nvarchar(10);not null"`
+	Category int `json:"category" gorm:"column:category;type:int;not null"`
+	// Color for the sensor, stored as a hex value (e.g., "#FF00FF")
+	Color string `json:"color" gorm:"column:color;type:nvarchar(7);not null"`
 	// Additional information about the sensor's functionality
 	Description string `json:"description" gorm:"column:description;type:nvarchar(255);"`
 	// Visibility: public (false) or private (true)
-	Visibility bool `json:"visibility" gorm:"column:visibility;type:bit;default:0"`
+	Visibility bool `json:"visibility" gorm:"column:visibility;type:bit;default:1"`
 	// UUID of the user who owns the sensor
 	SensorOwner uuid.UUID `json:"sensor_owner" gorm:"column:sensor_owner;type:uniqueidentifier;not null"`
 	// This field establishes a relationship between Sensor and User using the foreign key
