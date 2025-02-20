3b1a09fbb5340ec64b90445f620fdcb01c9630e5
dianarocha-eqs <diana.rocha@eqsglobal.com>
2025-02-20 17:11:27 +0000

    updated sensor data

--- src/api/internal/sensors_data/domain/sensors_data.go
+++ src/api/internal/sensors_data/domain/sensors_data.go
@@ -11,10 +11,10 @@ import (
 type SensorData struct {
 	// Unique identifier for the data entry
 	ID uuid.UUID `json:"id" gorm:"column:id;uniqueidentifier;not null"`
-	// Sensor that recorded this data
-	SensorID uuid.UUID `json:"sensor_id" gorm:"column:sensor_id;type:uniqueidentifier;not null"`
+	// UUID of the sensor that recorded this data
+	SensorUuid uuid.UUID `json:"sensorUuid" gorm:"column:sensorUuid;type:uniqueidentifier;not null"`
 	// This field establishes a relationship between Sensor and Sensor Data using the foreign key
-	Sensor domain.Sensor `json:"sensor" gorm:"foreignKey:SensorID;references:ID;constraint:OnDelete:CASCADE"`
+	Sensor domain.Sensor `json:"sensor" gorm:"foreignKey:SensorUuid;references:ID;constraint:OnDelete:CASCADE"`
 	// Timestamp when the data was recorded
 	Timestamp time.Time `json:"timestamp" gorm:"column:timestamp;type:datetime;not null;default:CURRENT_TIMESTAMP"`
 	// The measured value from the sensor
