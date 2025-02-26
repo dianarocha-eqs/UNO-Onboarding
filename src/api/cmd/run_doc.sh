41bc94498a1944171c7ffa1c15771a756d9b8d7c
dianarocha-eqs <diana.rocha@eqsglobal.com>
2025-02-26 13:35:08 +0000

    forgot some details

---
+++ src/api/cmd/run_doc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ 
+echo " - Navigate to root"
+cd ..
+ 
+if ! command -v swag &> /dev/null; then
+    echo " - Install swag"
+    go install github.com/swaggo/swag/cmd/swag@v1.16.3
+fi
+ 
+echo " - Export gopath"
+export PATH=$PATH:$HOME/go/bin
+ 
+echo " - Generate code using swag"
+swag init --parseDependency --overridesFile .swaggo
+ 
+echo " - Done!"
+
+ 
